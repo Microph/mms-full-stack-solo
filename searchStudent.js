e6e3ef31d24b85004a7b92e0948914db7f715c6d
Athip Intaraphirom <athip.int@gmail.com>
2017-10-29 15:26:47 +0700

    remove dup

--- searchStudent.js
+++
@@ -1,9 +0,0 @@
-func searchTutor() {
-    if is not tutor {
-        print("This area is only for tutors")
-        return
-    }
-
-    students = querySelectStudent(userInput.filter)
-    return students
-}
--- searchTutor.js
+++
@@ -1,9 +0,0 @@
-func searchTutor() {
-    if is not student {
-        print("This area is only for students")
-        return
-    }
-
-    courses = querySelectCourses(userInput.filter)
-    return courses
-}
