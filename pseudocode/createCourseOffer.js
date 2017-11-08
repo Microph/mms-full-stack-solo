f855c5dd36d6914561cdb74ddbf6a52a1aac4d3f
pakpoom <pakpoom.t@student.chula.ac.th>
2017-11-08 14:35:32 +0700

    change edit course offer

--- pseudocode/createCourseOffer.js
+++ pseudocode/createCourseOffer.js
@@ -1,12 +1,18 @@
-func createCourseOffer() {
-    if is not tutor {
-        print("This area is only for tutors.")
+func createCourseOffer(tutorID, subject, price, description, level) {
+    if tutorID == empty || subject == empty || price == empty || description == empty || level = empty {
+        print("กรุณากรอกข้อมูลให้ครบถ้วน")
+        return
+    } else if price < 150 {
+        print("กรุณากรอกราคามากกว่า 150 บาท/ชั่วโมง")
         return
     }
-
-    subject = userInput.subject
-    price = userInput.price
-    queryInsertSubject()
+    
+    createCourse = INSERT INTO 'Course'
+                    VALUES(tutorID = 'tutorID',
+                            subject = 'subject',
+                            price = 'price',
+                            description = 'description',
+                            level = 'level')
 
     return
 }
