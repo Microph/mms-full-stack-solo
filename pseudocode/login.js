a61f048139fb34cb25327964fcff65b05e78efad
pakpoom <pakpoom.t@student.chula.ac.th>
2017-11-08 15:15:30 +0700

    change login pseudocode

--- pseudocode/login.js
+++ pseudocode/login.js
@@ -1,14 +1,15 @@
-func login() {
-    if loginWithFacebook {
-        facebook = performFacebookLogin()
-        token = facebook.token
-        id = facebook.userID
+func login(loginType, username, password) {
+    if loginType == 'facebook' {
+        token, id = performFacebookLogin()
+    } else if loginType == 'line' {
+        token, id = performLineLogin()
     } else {
-        line = performLineLogin()
-        token = line.token
-        id = line.userID
+        id = SELECT username
+                FROM 'Admin'
+                WHERE username = 'username'
+                    AND password = 'password'
     }
 
-    querySelectUserID()
+    SESSIONS['token'], SESSION['id'] = token, id 
     return
 }
