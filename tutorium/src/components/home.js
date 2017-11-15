002d80742c3092b8190ca3e1ca3afcaa62d6fc14
Athip Intaraphirom <athip.int@gmail.com>
2017-11-15 13:51:09 +0700

    mock-up datalist homepage

--- tutorium/src/components/home.js
+++ tutorium/src/components/home.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import {
+  FlatButton,
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui";
 
 class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    var tutorList = [];
+    tutor.map(item => {
+      tutorList.push(<h3>{item.subject}</h3>);
+      item.name.map(person => {
+        tutorList.push(<h5>{person}</h5>);
+      })
+    });
+    return  tutorList ;
   }
 }
+
+const tutor = [
+  { subject: "math", name: ['Tutor #1', 'สอน เก่ง', 'ท็อป ทุกวิชา'] },
+  { subject: "eng", name: ['เมพขิง จิงกาเบล', 'ติวเตอร์ หมายเลขห้า', 'เก่ง ไปหมด'] }
+];
 
 export default Home;
