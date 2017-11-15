70f87b6e4c814c11ce4c643ca4fe32c5b9354041
pakpoom <pakpoom.t@student.chula.ac.th>
2017-11-15 17:01:25 +0700

    login with facebook via facebook-passport

--- tutorium/backend/api/user.js
+++ tutorium/backend/api/user.js
@@ -1,3 +1,5 @@
+const FacebookStrategy = require('passport-facebook').Strategy;
+
 //  users.js
 //
 //  Defines the users api. Add to a server by calling:
@@ -5,12 +7,54 @@
 'use strict';
 
 //  Only export - adds the API to the app with the given options.
-module.exports = (app, options) => {
-  
+module.exports = (app, passport, options) => {
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    options.repository.findUserByID(user.id, 'facebook').then((studentID) => {
+      if(studentID) {
+        return done(null, {registStatus: true, accountType: 'facebook', accountID:user.accountID})
+      } else {
+        return done(null, {registStatus: false, accountType: 'facebook', accountID:user.accountID})
+      }
+    })
+  });
+
+  passport.use(new FacebookStrategy({
+      clientID: '1585083688215887',
+      clientSecret: 'ee917ac7f9a30c4feda943772380509b',
+      callbackURL: "http://localhost:8123/api/auth/facebook/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        options.repository.findUserByID(profile.id, 'facebook').then((studentID) => {
+          if(studentID) {
+            return done(null, {registStatus: true, accountType: 'facebook', accountID:profile.id})
+          } else {
+            return done(null, {registStatus: false, accountType: 'facebook', accountID:profile.id})
+          }
+        })
+      })
+    }
+  ));
+
+  app.get('/api/auth/facebook', passport.authenticate('facebook'));
+
+  app.get('/api/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    (req, res) => {
+      // Successful authentication
+      res.status(200).send(req.user)
+    }
+  );
+
   app.post('/api/register', function (req, res, next) {
     if(req.body.agree) {
-      var userInfo = {
-                      accountID: req.body.id,
+      var userInfo = {accountType: req.body.accountType,
+                      accountID: req.body.accountID,
                       name: req.body.name,
                       surname: req.body.surname,
                       gender: req.body.gender,
@@ -20,41 +64,45 @@ module.exports = (app, options) => {
                       email: req.body.email,
                       mobile: req.body.mobile}
 
-      if(req.body.registerType == 'facebook') {
-        options.repository.registerByFacebook(userInfo).then((user) => {
-          if(!user) { 
-            res.status(200).send({
-              success: false,
-              msg: 'register incomplete'
-            });
-          } else {
-            res.status(200).send({
-              success: true,
-              studentID: 's-123456',
-              accountType: 'facebook',
-              accountID: '123456'
-            });
-          }
-        })
-        .catch(next);
-      } else {
-        options.repository.registerByLine(userInfo).then((user) => {
-          if(!user) { 
-            res.status(200).send({
-              success: false,
-              msg: 'register incomplete'
-            });
-          } else {
-            res.status(200).send({
-              success: true,
-              studentID: 's-123456',
-              accountType: 'line',
-              accountID: '654321'
-            });
-          }
-        })
-        .catch(next);
-      }
+      options.repository.register(userInfo).then((success) => {
+
+      })
+      .catch(next);
+      // if(req.body.registerType == 'facebook') {
+      //   options.repository.registerByFacebook(userInfo).then((user) => {
+      //     if(!user) { 
+      //       res.status(200).send({
+      //         success: false,
+      //         msg: 'register incomplete'
+      //       });
+      //     } else {
+      //       res.status(200).send({
+      //         success: true,
+      //         studentID: 's-123456',
+      //         accountType: 'facebook',
+      //         accountID: '123456'
+      //       });
+      //     }
+      //   })
+      //   .catch(next);
+      // } else {
+      //   options.repository.registerByLine(userInfo).then((user) => {
+      //     if(!user) { 
+      //       res.status(200).send({
+      //         success: false,
+      //         msg: 'register incomplete'
+      //       });
+      //     } else {
+      //       res.status(200).send({
+      //         success: true,
+      //         studentID: 's-123456',
+      //         accountType: 'line',
+      //         accountID: '654321'
+      //       });
+      //     }
+      //   })
+      //   .catch(next);
+      // }
     }
   })
   
