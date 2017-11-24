4b96d106e1abe0be5d0705e854d639a5f5f99d8c
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2017-11-24 23:49:41 +0700

    implement requests  manage page for admin

--- tutorium/front-end/src/admin-components/requestsmanage.js
+++ tutorium/front-end/src/admin-components/requestsmanage.js
@@ -1,11 +1,117 @@
 import React, { Component } from 'react';
+import {
+  FlatButton,  
+} from "material-ui";
 
 class RequestsManage extends Component {
-    render() {
-        return (
-            <h1>RequestsManage</h1>
-        );
-    }
+  genCard(){
+      return(<RequestCard />);
+  }
+
+  render() {
+      return(
+        <div>
+          {this.genCard()}
+          {this.genCard()}
+          {this.genCard()}
+        </div>
+      );
+  }
+}
+
+class RequestCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFullDetail: false,
+    };
+
+    this.onClickShowHide = this.onClickShowHide.bind(this);
+  }
+
+  onClickShowHide(){
+    const prev = this.state.showFullDetail;
+    this.setState({showFullDetail: !prev});
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="row">
+          <div class="thumbnail clearfix">
+            <div class="container">
+                <div class="row">
+                  <div class="col-sm-7 col-md-7">
+                      <h2>เด็กดี ตั้งใจเรียน</h2>
+                  </div>
+                  <div class="col-sm-5 col-md-5">
+                      <h2 align="right">ID: 000023</h2>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-sm-7 col-md-7">
+                      <h4>เด็กดี ตั้งใจเรียน</h4>
+                  </div>
+                  <div class="col-sm-5 col-md-5">
+                      <h4 align="right">ID: 000023</h4>
+                  </div>
+                </div>
+                
+                <div class="row">
+                  <div class="col-sm-7 col-md-7">
+                      <h4>เด็กดี ตั้งใจเรียน</h4>
+                  </div>
+                  <div class="col-sm-5 col-md-5">
+                      <h4 align="right">ID: 000023</h4>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-sm-7 col-md-7">
+                      <h4>เด็กดี ตั้งใจเรียน</h4>
+                  </div>
+                  <div class="col-sm-5 col-md-5">
+                      <h4 align="right">ID: 000023</h4>
+                  </div>
+                </div>
+
+                <div hidden={!this.state.showFullDetail}>
+                  <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <h4>เด็กดี ตั้งใจเรียน</h4>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <h4>เด็กดี ตั้งใจเรียน</h4>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="row">
+                  <div class="col-sm-12 col-md-12" align="center">
+                  <FlatButton
+                    onClick={this.onClickShowHide}
+                    style={{
+                      width: "100%",
+                      color: "#fff",
+                      margin: "auto",
+                      marginTop: 20,
+                      backgroundColor: "gray"
+                    }}
+                    labelStyle={{ fontSize: 15, fontWeight: 700 }}
+                    label="ดูเพิ่มเติม"
+                  />
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RequestsManage;
