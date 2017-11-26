d18413d2722ad236e3563019c283090f3c8923ed
5730282621-NV <keenesis@gmail.com>
2017-11-26 13:32:21 +0700

    minor bug

--- tutorium/back-end/src/api/report.js
+++ tutorium/back-end/src/api/report.js
@@ -4,13 +4,20 @@ module.exports = (app, passport, options) => {
 
     //------------------------------------------------------------------------------------------------------------------
     app.post('/api/user-write-report', (req, res, next) => {
-        let qry = require('../repository/report')
-        qry.userWriteReport(req.body.reporterStudentID, req.body.reportedStudentID, req.body.topic, req.body.detail).then((result) => {
-            res.status(200).send({ 
-                success: true, 
-                result: result
+        if (!req.body.reporterStudentID){
+            res.status(500).send({ 
+                success: false, 
+                msg: 'Bugged!!'
             })
-        })
+        } else {
+        let qry = require('../repository/report')
+            qry.userWriteReport(req.body.reporterStudentID, req.body.reportedStudentID, req.body.topic, req.body.detail).then((result) => {
+                res.status(200).send({ 
+                    success: true, 
+                    result: result.reportID
+                })
+            })
+        }
     })
 
 }
